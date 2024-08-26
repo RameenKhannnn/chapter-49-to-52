25e603b1ffa4b3d8439474d78eb90d1784453243
RameenKhannnn <rameenkhan8787@gmail.com>
2024-08-26 17:52:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// ==================== Radio button value get ============
+
+// function selectJuice(){
+//     var radio = document.getElementsByName('juice')
+//     for(var i = 0; i < radio.length ; i++){
+//         // console.log(radio[i].checked);
+//         if(radio[i].checked === true){
+//             console.log(radio[i].value);
+//         }
+//     }
+    
+// }
+
+//===================== onfocus ================== 
+// function color(event){
+//     event.target.style.backgroundColor = 'grey'
+// }
+
+//   ================== Onblur ==================
+// function getCity (){
+//     var zip = document.getElementById('zip')
+//     console.log(zip);
+//     var city = document.getElementById('city')
+//     switch(Number(zip.value)){
+//         case 11:
+//             city.value = 'Karachi'
+//             break;
+//             case 22:
+//                 city.value = 'Quetta'
+//                 break;
+//                 case 33:
+//                     city.value = 'Lahore'
+//                     break;
+
+//                     default:
+//                         city.value =''
+//                         console.log('not found')
+//     }
+// }
+
+
+
+// keypress
+// var list = document.getElementById('list')
+// function getButton (event){
+//     // console.log(event.keyCode);
+//     if(event.keyCode === 13){
+//         list.innerHTML += '<li>' + event.target.value +'</li>'
+//         event.target.value = ''
+//     }
+// }
+
+// function checkAddress(fieldId) {
+//      if (document.getElementById(fieldId).value === "") {
+//      alert("Email address required.");
+//      }
+// }
+
+//  function expandLoris() {
+//   var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which     make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of
+//     distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several
+//     adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.
+//     Slow lorises have a toxic bite, a rare trait among mammals"
+
+
+//  }
+
